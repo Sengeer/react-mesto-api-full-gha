b6e7d6adf31bea12a3b3e7e4d73f4adbccd1f843
Sengeer <sengeer@mail.ru>
2023-09-27 21:23:25 +0300

    feat: реализовать аутентификация на cookie

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,6 @@ mongoose.connect(URL, {
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://localhost:3001',
     'http://mesto.sengeer.nomoredomainsrocks.ru',
     'https://mesto.sengeer.nomoredomainsrocks.ru',
   ],
