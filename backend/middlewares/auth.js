73424ed93add2334194cccf842bd349d1460e78d
Sengeer <sengeer@mail.ru>
2023-09-27 14:11:14 +0300

    hotfixes: убрать оператор throw в мидлвэре auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,14 +7,16 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'uXDm8ygPYPzEJ3qbycmZ');
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
   req.user = payload;
   next();
