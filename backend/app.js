3f4c508d2d698aff5d7733c2cc8b2094dbff86b6
Sengeer <sengeer@mail.ru>
2023-09-28 14:44:57 +0300

    feat: поменять порт приложения

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const errorer = require('./middlewares/errorer');
 
 const {
-  PORT = 3001,
+  PORT = 3000,
   URL = 'mongodb://127.0.0.1:27017/mestodb',
 } = process.env;
 const app = express();
